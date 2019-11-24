d4ead8262d9db212fc0820f3448101dd02357546
*zpdrynR76K <linhvtm@tech.est-rouge.com>
2019-11-24 11:48:55 +0700

    add city management

--- resources/js/pages/backend/cities/Show.jsx
+++ resources/js/pages/backend/cities/Show.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 import RouteConst from '../../../constants/Route';
-import CategoriesRequests from '../../../requests/backend/CategoriesRequests';
+import CitiesRequests from '../../../requests/backend/CitiesRequests';
 
 export default class extends Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.state={
             id: this.props.match.params.id,
             info: {}
         };
@@ -16,40 +16,40 @@ export default class extends Component {
         this.getInfo(this.state.id);
     }
 
-    getInfo = (id) => {
-        CategoriesRequests.showByID(id).then((response) => {
+    getInfo=(id) => {
+        CitiesRequests.showByID(id).then((response) => {
             console.log(response)
-            if (response.meta.status === 200) {
-                this.setState({ info: response.data });
+            if(response.meta.status===200) {
+                this.setState({info: response.data});
             } else {
-                this.props.history.push(RouteConst.backEnd.categories.index.path);
+                this.props.history.push(RouteConst.backEnd.cities.index.path);
             }
         });
     }
 
     render() {
-        const breadcrumbElement = (
+        const breadcrumbElement=(
             <ol className="breadcrumb">
                 <li className="breadcrumb-item">
                     <Link to={RouteConst.backEnd.home.index.path}>Home</Link>
                 </li>
                 <li className="breadcrumb-item">
-                    <Link to={RouteConst.backEnd.categories.index.path}>Categories</Link>
+                    <Link to={RouteConst.backEnd.cities.index.path}>Cities</Link>
                 </li>
                 <li className="breadcrumb-item active">Show</li>
             </ol>
         );
 
-        const linkElement = (
+        const linkElement=(
             <div>
-                <Link to={`${RouteConst.backEnd.categories.index.path}/${this.state.info.id}/edit`}
+                <Link to={`${RouteConst.backEnd.cities.index.path}/${this.state.info.id}/edit`}
                     className="btn btn-primary">Edit</Link>
-                <Link to={RouteConst.backEnd.categories.index.path}
+                <Link to={RouteConst.backEnd.cities.index.path}
                     className="btn btn-secondary ml-2">Go back</Link>
             </div>
         );
 
-        const infoElement = (
+        const infoElement=(
             <div>
                 <div className="row justify-content-md-center">
                     <div className="col-sm-12 col-md-8 show-info-container">
@@ -60,16 +60,6 @@ export default class extends Component {
                         <div className="show-info">
                             <p className="show-label">Name</p>
                             <p className="show-value">{this.state.info.name}</p>
-                        </div>
-                        <div className="show-info">
-                            <p className="show-label">Image</p>
-                            <p className="show-value">
-                                <img src={this.state.info.image} className="img-fluid"/>
-                            </p>
-                        </div>
-                        <div className="show-info">
-                            <p className="show-label">Description</p>
-                            <p className="show-value">{this.state.info.description}</p>
                         </div>
                         <div className="show-info">
                             <p className="show-label">Created at</p>
@@ -91,7 +81,7 @@ export default class extends Component {
 
                 <div className="card mb-3">
                     <div className="card-header">
-                        <i className="fas fa-table"></i> Info category
+                        <i className="fas fa-table"></i> Info city
                     </div>
                     <div className="card-body">
                         {infoElement}
